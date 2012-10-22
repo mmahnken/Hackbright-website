40d0e065ed9d6872e0bd9a4e90449c8c23cf8b6e
David J. Phillips <d@sutterstudios.com>
2012-10-21 20:07:13 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackbrightWebsite::Application.config.secret_token = '7e2d256507e39a0654cb1f9b3a85932ed7f2e599d1effd0f664024e91714c8f3bacbf166fc47094f684159cf83a6e0dfd2582ead97b99796e7e905dec034e19e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HackbrightWebsite::Application.config.session_store :cookie_store, key: '_HackbrightWebsite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HackbrightWebsite::Application.config.session_store :active_record_store
