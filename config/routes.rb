5fc70b46c7607172430304c609d9a9282e7f7cab
David J. Phillips <d@sutterstudios.com>
2013-09-06 22:32:47 -0700

    added login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 HackbrightWebsite::Application.routes.draw do
   
 
+  devise_for :users
+  
+  devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
+
   match "/visitor/create" => "visitor#create", :via => 'post' 
 
   get "/home" => "pages#home"
